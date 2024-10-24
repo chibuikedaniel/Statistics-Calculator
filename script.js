400b4feef046c15b375d24f875aa9e8a7989b4fd
dracuzi <danielchibuike77@gmail.com>
2024-10-24 23:20:26 +0100

    added a function to calculate the mean

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
+// const getMean = (array) => {
+//     const sum = array.reduce((acc, el) => acc + el, 0);
+//     const mean = sum / array.length;
+//     return mean;
+// }
+
+// using the implicit return to get the mean
+const getMean = (array) => array.reduce((acc, el) => acc + el, 0) / array.length;
+
+
 const calculate = () => {
     const value = document.querySelector("#numbers").value;
     const array = value.split(/,\s*/g);
     const numbers = array.map(el => Number(el)).filter(el => !isNaN(el));
+
+    const mean = getMean(numbers);
+    document.querySelector("#mean").textContent = mean;
 }
